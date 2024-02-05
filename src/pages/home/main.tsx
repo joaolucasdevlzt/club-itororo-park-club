8028502e3e629b66de69ac4c79de77298d4af849
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2024-02-05 15:46:36 -0300

    fix: removed unused code

--- src/pages/home/main.tsx
+++ src/pages/home/main.tsx
@@ -1,15 +1,11 @@
-import { Typography } from '@mui/material';
 import { Helmet } from 'react-helmet-async';
-import SectionWrapper from 'src/components/section-wrapper';
 
 // ----------------------------------------------------------------------
 
 export default function HomePage() {
   return (
-    <>
-      <Helmet>
+    <Helmet>
         <title> Lazertur</title>
       </Helmet>
-    </>
   );
 }

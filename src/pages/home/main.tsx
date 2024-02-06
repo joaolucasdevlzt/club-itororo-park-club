9d6958c02a4d6b2dfb346f29348aebadf8eeee15
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2024-02-06 17:05:46 -0300

    chore: reusable banner component

--- src/pages/home/main.tsx
+++ src/pages/home/main.tsx
@@ -1,11 +1,19 @@
 import { Helmet } from 'react-helmet-async';
 
+import Banner from 'src/components/banner';
+
 // ----------------------------------------------------------------------
 
 export default function HomePage() {
   return (
-    <Helmet>
-      <title> Lazertur</title>
-    </Helmet>
+    <>
+      <Helmet>
+        <title> Lazertur</title>
+      </Helmet>
+      <Banner
+        images={['/assets/images/contact/banner_topo.jpg', '/assets/images/contact/hero.jpg']}
+      />
+     
+    </>
   );
 }

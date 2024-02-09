4e841e667205077ab35bde8fca5b51b476b1aeb0
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2024-02-09 14:59:06 -0300

    chore: call to action card component

---
+++ src/components/cta-card/types.ts
@@ -0,0 +1,10 @@
+import { SxProps } from '@mui/material';
+
+export interface CTACardInterface {
+  icon: string;
+  iconProps?: SxProps;
+  text: String;
+  cardProps?: SxProps;
+  buttonText: string;
+  buttonProps?: SxProps;
+}

04e54880474360aff0a963ce288ed194877ef0ee
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2024-02-10 12:13:25 -0300

    chore: added title to most wanted section

--- src/sections/most-wanted-destinations/index.tsx
+++ src/sections/most-wanted-destinations/index.tsx
@@ -1,13 +1,28 @@
-import { Box, Grid } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 
 import Image from 'src/components/image';
 
 export default function MostWantedDestinations() {
   return (
     <Box>
+      <Typography
+        sx={{
+          fontSize: { xs: 30, sm: 40 },
+          fontFamily: 'Prompt-Medium',
+          fontWeight: '600',
+          color: '#28327F',
+        }}
+      >
+        Destinos mais procurados
+      </Typography>
       <Grid
         container
-        sx={{ display: 'flex', justifyContent: 'space-between', gap: { xs: 3, md: 0 } }}
+        sx={{
+          marginTop: 6,
+          display: 'flex',
+          justifyContent: 'space-between',
+          gap: { xs: 3, md: 0 },
+        }}
       >
         <Grid item xs={12} md={5.9}>
           <Image image="maceio" title="Maceió" />
@@ -18,13 +33,13 @@ export default function MostWantedDestinations() {
       </Grid>
       <Grid container sx={{ display: 'flex', justifyContent: 'space-between', marginTop: 3 }}>
         <Grid item xs={3.9}>
-          <Image image="gramado" title="Gramado" aspectRatio='4/4' />
+          <Image image="gramado" title="Gramado" aspectRatio="4/4" />
         </Grid>
         <Grid item xs={3.9}>
-          <Image image="orlando" title="Orlando" aspectRatio='4/4' />
+          <Image image="orlando" title="Orlando" aspectRatio="4/4" />
         </Grid>
         <Grid item xs={3.9}>
-          <Image image="bariloche" title="Bariloche" aspectRatio='4/4' />
+          <Image image="bariloche" title="Bariloche" aspectRatio="4/4" />
         </Grid>
       </Grid>
     </Box>

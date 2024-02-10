af082a0766562c7f8072ef12cea38dcd9ed147ea
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2024-02-10 11:34:53 -0300

    fix: aspect ratio shall be optional

--- src/components/image/types.tsx
+++ src/components/image/types.tsx
@@ -2,5 +2,5 @@ export interface ImageInterface {
   titlePosition?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
   image: string;
   title?: string;
-  aspectRatio: string;
+  aspectRatio?: string;
 }

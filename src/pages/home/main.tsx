2fdc5ae1cb151313c0cb78a4e253b799a8eac99c
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2024-02-12 17:42:38 -0300

    chore: added grop companies section to homepage

--- src/pages/home/main.tsx
+++ src/pages/home/main.tsx
@@ -6,6 +6,7 @@ import SectionWrapper from 'src/components/section-wrapper';
 import ContactsSection from 'src/sections/contact';
 import AboutUsSection from 'src/sections/about-us';
 import StayInTouch from 'src/sections/stay-in-touch';
+import GroupCompanies from 'src/sections/group-companies';
 import HighlightPackageSection from 'src/sections/highlight-package';
 import MostWantedDestinations from 'src/sections/most-wanted-destinations';
 
@@ -34,6 +35,9 @@ export default function HomePage() {
       </SectionWrapper>
       <SectionWrapper>
         <AboutUsSection />
+      </SectionWrapper>
+      <SectionWrapper>
+        <GroupCompanies />
       </SectionWrapper>
     </>
   );

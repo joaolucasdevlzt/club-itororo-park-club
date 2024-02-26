e021ed92769c42cf9cbfcfb0725a6f5dd75efd89
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2024-02-26 16:46:01 -0300

    component: added simple title and text component

---
+++ src/components/title-and-text/index.tsx
@@ -0,0 +1,46 @@
+import { Box, Typography } from '@mui/material';
+
+interface titleAndTextsProps {
+  title: string;
+  texts: string | string[];
+}
+export default function TitleAndTexts({ title, texts }: titleAndTextsProps) {
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
+      <Typography
+        sx={{
+          fontSize: '26px',
+          lineHeight: 1.3,
+          fontFamily: 'Prompt-SemiBold',
+          color: (t) => t.palette.secondary.main,
+        }}
+      >
+        Quem somos
+      </Typography>
+
+      {Array.isArray(texts) &&
+        texts.map((text) => (
+          <Typography
+            sx={{
+              fontSize: '18px',
+              fontFamily: 'Kanit-Light',
+              color: (t) => t.palette.secondary.main,
+            }}
+          >
+            {text}
+          </Typography>
+        ))}
+      {typeof texts === 'string' && (
+        <Typography
+          sx={{
+            fontSize: '18px',
+            fontFamily: 'Kanit-Light',
+            color: (t) => t.palette.secondary.main,
+          }}
+        >
+          {texts}
+        </Typography>
+      )}
+    </Box>
+  );
+}

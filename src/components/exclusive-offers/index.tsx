361b39d2f67d0e102f8067f12f262527bbdba040
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2024-03-01 09:58:48 -0300

    fix: stay in touch section

--- src/components/exclusive-offers/index.tsx
+++ src/components/exclusive-offers/index.tsx
@@ -10,9 +10,10 @@ export default function ExclusiveOffers() {
         backgroundPosition: { xs: 'right', md: 'center' },
         backgroundRepeat: 'no-repeat',
         backgroundImage: `url(/assets/background/lazertur/especial_offers.svg)`,
-        aspectRatio: { xs: '4/3', md: '3/4' },
+        aspectRatio: { xs: '4/3', md: '3/3.5' },
         borderRadius: 3,
-        padding: { xs: 3, sm: 10, md: 5 },
+        px: { xs: 3, lg: 0 },
+        py: { xs: 3, lg: 0 },
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
@@ -26,11 +27,19 @@ export default function ExclusiveOffers() {
         Quer receber ofertas exclusivas?
       </Typography>
       <Typography
-        sx={{ fontFamily: 'Kanit-ExtraLight', fontSize: 20, textAlign: 'center', color: 'white' }}
+        sx={{
+          pl: 5,
+          pr: 5,
+          fontFamily: 'Kanit-ExtraLight',
+          fontSize: 20,
+          textAlign: 'center',
+          color: 'white',
+        }}
       >
         Participe do nosso grupo VIP no WhatsApp e seja o primeiro a saber das melhores ofertas.
       </Typography>
       <StyledButton
+        sx={{ maxWidth: 'fit-content', pl: 5, pr: 5 }}
         text="Clique aqui para participar"
         callback={() => window.open('https://chat.whatsapp.com/Iux6c5E21nW10cHA2R2jAv', '-_blank')}
       />
@@ -46,7 +55,7 @@ export default function ExclusiveOffers() {
       </Typography>
       <Box
         sx={{
-          width: { xs: 200, md: '50%' },
+          width: { xs: 180, md: '40%' },
           backgroundSize: 'contain',
           backgroundRepeat: 'no-repeat',
           backgroundImage: `url(/assets/images/lazertur/qr_code.svg)`,

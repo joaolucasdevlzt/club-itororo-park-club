9100bfa5fd2de1b0d3b13804877e578ef1d4e8c4
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2024-04-08 15:43:01 -0300

    chore: banner

---
+++ src/pages/services/index.tsx
@@ -0,0 +1,26 @@
+import { Helmet } from 'react-helmet-async';
+
+import Banner from 'src/components/banner';
+import SectionWrapper from 'src/components/section-wrapper';
+
+import TitleTextAndCTA from 'src/sections/title-text-cta';
+
+export default function Services() {
+  return (
+    <>
+      <Helmet>
+        {process.env.NODE_ENV === 'development' && (
+          <meta name="robots" content="noindex, nofollow" />
+        )}
+        <title>Lazertur - Serviços</title>
+      </Helmet>
+      <Banner images="assets/images/lazertur/services/banner_services.png" />
+      <SectionWrapper>
+        <TitleTextAndCTA
+          title="Seguro de viagem"
+          text="Tanto nas suas viagens nacionais quanto internacionais, você precisa de cuidados essenciais, então escolha um seguro viagem. Na Lazertur Viagens, você encontra as melhores opções de seguro para sua viagem e sua saúde. Adquira sua viagem e seu seguro, tudo em um só lugar, e viaje com toda a tranquilidade que a Lazertur pode te oferecer."
+        />
+      </SectionWrapper>
+    </>
+  );
+}

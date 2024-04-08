f34128d4d775d4b8a400a4dcac00e7e0af1f89a5
vpedraLZT <146345334+vpedraLZT@users.noreply.github.com>
2024-04-08 16:11:56 -0300

    chore: second section

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 
 import Banner from 'src/components/banner';
+import IconTextCard from 'src/components/icon-text-card';
 import SectionWrapper from 'src/components/section-wrapper';
 
 import TitleTextAndCTA from 'src/sections/title-text-cta';
@@ -20,6 +21,9 @@ export default function Services() {
           title="Seguro de viagem"
           text="Tanto nas suas viagens nacionais quanto internacionais, você precisa de cuidados essenciais, então escolha um seguro viagem. Na Lazertur Viagens, você encontra as melhores opções de seguro para sua viagem e sua saúde. Adquira sua viagem e seu seguro, tudo em um só lugar, e viaje com toda a tranquilidade que a Lazertur pode te oferecer."
         />
+      </SectionWrapper>
+      <SectionWrapper>
+        <IconTextCard />
       </SectionWrapper>
     </>
   );
